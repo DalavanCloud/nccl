9c271e38ffc13109e3120c8ed0d6275a9bc895b5
Sylvain Jeaugey <sjeaugey@nvidia.com>
2016-04-29 09:20:01 -0700

    Remove nvToolsExt dependency in src/

--- src/all_reduce.cu
+++ src/all_reduce.cu
@@ -26,7 +26,6 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  ************************************************************************/
 
-#include <nvToolsExt.h>
 #include <assert.h>
 
 #include "core.h"
