b1b7cad7e0cd98b15464be247f3ab9467efd2e3f
anshumang <goswami.anshuman@gmail.com>
2017-03-01 20:19:00 -0800

    Ping pong socket test added - tested compilation only

--- test/unit/ncclNet_test.cu
+++ test/unit/ncclNet_test.cu
@@ -5,11 +5,73 @@
  ************************************************************************/
 
 #include <stdio.h>
+#include <string.h>
 #include "mpi.h"
 #include "nccl.h"
 
 extern ncclNet_t ncclNetSocket; 
 extern ncclNet_t ncclNetIb; 
+
+int Socket_tester(ncclNet_t *net, int dev, int rank, int nranks){
+  if(!rank) printf("Socket tester\n");
+
+  int failed = 0;
+  if(rank==0){
+    for(int rnk=1; rnk<nranks; rnk++){
+      char listenHandle[NCCL_NET_HANDLE_MAXSIZE];
+      char *listenComm; 
+      if(net->listen(0, (void *)listenHandle, (void **)&listenComm)){ failed=1; goto out; }
+
+      char *recvComm;
+      if(net->accept(listenComm, (void **)&recvComm)){ failed=1; goto out; }
+
+      if(net->closeListen(listenComm)){ failed=1; goto out; }
+
+      /*ping*/
+      char data;
+      int type = 0;
+      type |= NCCL_PTR_HOST;
+      char *request;
+      if(net->irecv(recvComm, &data, sizeof(char), type, (void **)&request)){ failed=1; goto out; }
+
+      int done=0;
+      do {
+        int size = -1;
+        if(net->test(request, &done, &size)){ failed=1; goto out; }
+      } while(!done);
+      if(net->closeRecv(recvComm)){ failed=1; goto out; }
+    }
+  }else{
+    char connectHandle[NCCL_NET_HANDLE_MAXSIZE];
+    char *sendComm;
+    if(net->connect(0, connectHandle, (void **)&sendComm)){ failed=1; goto out; }
+
+    /*pong*/  
+    char data;
+    int type = 0;
+    type |= NCCL_PTR_HOST;
+    char *request;
+    if(net->isend(sendComm, &data, sizeof(char), type, (void **)&request)){ failed=1; goto out; };
+
+    int done=0;
+    do {
+      int size = -1;
+      if(net->test(request, &done, &size)){ failed=1; goto out; }
+    } while(!done);
+    if(net->closeSend(sendComm)){ failed=1; goto out; }
+  } 
+
+out:
+  return failed;
+}
+
+int IB_tester(ncclNet_t *net, int dev, int rank, int nranks){
+  int failed = 0;  
+  if(!rank) printf("IB tester\n");
+  return failed;
+}
+
+int (*testers[]) (ncclNet_t *, int, int, int) = {&Socket_tester, &IB_tester};
 
 int main(int argc, char *argv[]) {
   int nranks, rank;
@@ -22,45 +84,16 @@ int main(int argc, char *argv[]) {
 
   MPI_Barrier(MPI_COMM_WORLD);
 
+  int failed = 0;
   ncclNet_t *nets[] = {&ncclNetSocket, &ncclNetIb};
   for(int i=0; i<sizeof(nets)/sizeof(nets[0]); i++){
     ncclNet_t *net = nets[i]; 
     if(!rank){
       printf("ncclNet implementation %s found \n", net->name);
     }
+    failed = testers[i](net, 0, rank, nranks); /*TODO: when NIC is not dev0*/
+    if (failed) goto out;
   }
-  int failed = 0;
-#if 0
-  if(rank==0){
-    for(int rnk=1; rnk<nranks; rnk++){
-      if(net->listen()){
-         goto out;
-      }
-      if(net->accept()){
-         goto out;
-      }
-      net->closeListen();
-      /*ping*/
-      net->irecv();
-      int done=0;
-      do {
-        net->test(&done);
-      } while(!done);
-      net->closeRecv();
-    }
-  }else{
-    if(net->onnect()){
-       goto out;
-    }
-    /*pong*/  
-    net->isend();
-    int done=0;
-    do {
-      net->test(&done);
-    } while(!done);
-    net->closeSend();
-  } 
-#endif
   MPI_Finalize();
 
 out:
