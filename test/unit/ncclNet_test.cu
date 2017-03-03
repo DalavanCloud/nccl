ae52c1922ac17ec5c5e18ec8d4e81204e76cb3d5
anshumang <goswami.anshuman@gmail.com>
2017-03-02 20:23:05 -0800

    net MPI - WIP

--- test/unit/ncclNet_test.cu
+++ test/unit/ncclNet_test.cu
@@ -13,6 +13,7 @@
 
 extern ncclNet_t ncclNetSocket; 
 extern ncclNet_t ncclNetIb; 
+extern ncclNet_t ncclMpi; 
 
 int Socket_tester(ncclNet_t *net, char *data, size_t bytes, size_t *duration, int dev, int rank, int nranks, MPI_Comm comm){
   if(!rank) printf("Socket tester\n");
@@ -161,7 +162,88 @@ out:
   return failed;
 }
 
-int (*testers[]) (ncclNet_t *, char *, size_t, size_t *, int, int, int, MPI_Comm) = {&Socket_tester, &IB_tester};
+extern "C"
+void ncclMpiHook(MPI_Comm comm);
+
+int MPI_tester(ncclNet_t *net, char *data, size_t bytes, size_t *duration, int dev, int rank, int nranks, MPI_Comm comm){
+  if(!rank) printf("MPI tester\n");
+
+  ncclMpiHook(MPI_COMM_WORLD);
+  int failed = 0;
+  if(rank==0){
+    for(int rnk=1; rnk<nranks; rnk++){
+      char listenHandle[NCCL_NET_HANDLE_MAXSIZE];
+      char *listenComm; 
+      //printf("%d listen\n", rank);
+      if(net->listen(0, (void *)listenHandle, (void **)&listenComm)){ failed=1; goto out; }
+
+      //printf("%d MPI_Send\n", rank);
+      if(MPI_Send(listenHandle, NCCL_NET_HANDLE_MAXSIZE, MPI_BYTE, rnk, 0, comm)){ failed=1; goto out; }
+
+      //printf("%d accept\n", rank);
+      char *recvComm;
+      if(net->accept(listenComm, (void **)&recvComm)){ failed=1; goto out; }
+
+      //printf("%d closeListen\n", rank);
+      if(net->closeListen(listenComm)){ failed=1; goto out; }
+
+      /*ping*/
+      int type = 0;
+      type |= NCCL_PTR_HOST;
+      char *request;
+      //printf("%d recv\n", rank);
+      struct timeval start, end;
+      gettimeofday(&start, NULL);
+      if(net->irecv(recvComm, data, bytes, type, (void **)&request)){ failed=1; goto out; }
+
+#if 0
+      int done=0;
+      do {
+        int size = -1;
+        if(net->test(request, &done, &size)){ failed=1; goto out; }
+      } while(!done);
+      gettimeofday(&end, NULL);
+      *duration = (end.tv_sec - start.tv_sec)*1000000 + (end.tv_usec - start.tv_usec);
+      //printf("%d closeRecv\n", rank);
+      if(net->closeRecv(recvComm)){ failed=1; goto out; }
+#endif
+    }
+  }else{
+    char connectHandle[NCCL_NET_HANDLE_MAXSIZE];
+    char *sendComm;
+    //printf("%d MPI_Recv\n", rank);
+    if(MPI_Recv(connectHandle, NCCL_NET_HANDLE_MAXSIZE, MPI_BYTE, 0, 0, comm, MPI_STATUS_IGNORE)){ failed=1; goto out; }
+
+    //printf("%d connect\n", rank);
+    if(net->connect(0, connectHandle, (void **)&sendComm)){ failed=1; goto out; }
+
+    /*pong*/  
+    int type = 0;
+    type |= NCCL_PTR_HOST;
+    char *request;
+    //printf("%d send\n", rank);
+    struct timeval start, end;
+    gettimeofday(&start, NULL);
+    if(net->isend(sendComm, data, bytes, type, (void **)&request)){ failed=1; goto out; };
+
+#if 0
+    int done=0;
+    do {
+      int size = -1;
+      if(net->test(request, &done, &size)){ failed=1; goto out; }
+    } while(!done);
+    gettimeofday(&end, NULL);
+    *duration = (end.tv_sec - start.tv_sec)*1000000 + (end.tv_usec - start.tv_usec);
+    //printf("%d closeSend\n", rank);
+    if(net->closeSend(sendComm)){ failed=1; goto out; }
+#endif
+  } 
+
+out:
+  return failed;
+}
+
+int (*testers[]) (ncclNet_t *, char *, size_t, size_t *, int, int, int, MPI_Comm) = {&Socket_tester, &IB_tester, &MPI_tester};
 
 #define MAX_SIZE (1024*1024*1024)
 
@@ -181,8 +263,8 @@ int main(int argc, char *argv[]) {
 
   int failed = 0;
   char *data = new char[MAX_SIZE];
-  ncclNet_t *nets[] = {&ncclNetSocket, &ncclNetIb};
-  for(int i=1; i<sizeof(nets)/sizeof(nets[0]); i++){
+  ncclNet_t *nets[] = {&ncclNetSocket, &ncclNetIb, &ncclMpi};
+  for(int i=2; i<sizeof(nets)/sizeof(nets[0]); i++){
     ncclNet_t *net = nets[i]; 
     if(!rank){
       printf("ncclNet implementation %s found \n", net->name);
@@ -195,8 +277,9 @@ int main(int argc, char *argv[]) {
       failed = testers[i](net, data, b, &d, 0, rank, nranks, MPI_COMM_WORLD); /*TODO: when NIC is not dev0*/
       gettimeofday(&end, NULL);
       size_t duration = (end.tv_sec - start.tv_sec)*1000000 + (end.tv_usec - start.tv_usec);
-      if(!rank) {printf("Duration (total) %lld us duration (data transfer) %lld us Bandwidth %lld MB/s\n", duration, d, b/d);}
+      //if(!rank) {printf("Duration (total) %lld us duration (data transfer) %lld us Bandwidth %lld MB/s\n", duration, d, b/d);}
       if (failed) goto out;
+      break;
     }
   }
   delete data;
