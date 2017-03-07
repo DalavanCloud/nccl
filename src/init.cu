DalavanCloud/nccl

commit 6bf78c568c5ef54c84e56d498e902d9bb928bbab

Added cudaGetDeviceCount; Fixes NCCL-66

src/init.cu
@@ -485,7 +485,9 @@ ncclResult_t ncclCommInitAll(ncclComm_t* comms, int ndev, const int* devlist) {
   showVersion();
 
   NCCLCHECK(PtrCheck(comms, "CommInitAll", "comms"));
-  if (ndev < 1) {
+  int devcount=0;
+  cudaGetDeviceCount(&devcount);
+  if (ndev < 1 || ndev > devcount) {
     WARN("Invalid device count requested : %d", ndev);
     return ncclUnsupportedDeviceCount;
   }

test/apitest/ncclCommInitAll_test.cu
@@ -55,7 +55,7 @@ TEST_F(ncclCommInitAll_test, DISABLED_ndev_toomany) {
         pcomms = NULL;
     }
 };
-#if 0 // useless?
+#if 1 // useless?
 TEST_F(ncclCommInitAll_test, ndev_toomany_and_devList_allZero) {
     int ndev = 100;
     comms = (ncclComm_t*)calloc(ndev, sizeof(ncclComm_t));

commit a49bc6879990f9de8f3afc33d8e9f4e3342c010f

fixing a regression
introduced in 6bf78c568c5ef54c84e56d498e902d9bb928bbab

src/init.cu
@@ -400,14 +400,16 @@ ncclResult_t ncclCommInitRank(ncclComm_t* newcomm, int ndev, ncclUniqueId commId
   if (myrank == 0) showVersion();
 
   NCCLCHECK(PtrCheck(newcomm, "CommInitRank", "newcomm"));
-  int cudaDev;
-  CUDACHECK(cudaGetDeviceCount(&cudaDev));
-  if (ndev < 1 || ndev > cudaDev) {
+  int cudaDevCount;
+  CUDACHECK(cudaGetDeviceCount(&cudaDevCount));
+  if (ndev < 1 || ndev > cudaDevCount) {
     WARN("Invalid device count requested : %d", ndev);
     return ncclUnsupportedDeviceCount;
   }
 
   if (ncclAsyncMode()) {
+    int cudaDev;
+    CUDACHECK(cudaGetDevice(&cudaDev));
     return ncclAsyncInit(ncclCommInitRankSync, cudaDev, newcomm, ndev, commId, myrank);
   } else {
     return ncclCommInitRankSync(newcomm, ndev, commId, myrank);