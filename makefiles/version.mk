5f17bc37c4b7dcb9b0c8688f2877893439b385af
Sylvain Jeaugey <sjeaugey@nvidia.com>
2017-04-17 15:00:36 -0700

    Add CUDA build in Debian package

--- makefiles/version.mk
+++ makefiles/version.mk
@@ -2,8 +2,9 @@
 NCCL_MAJOR   := 2
 NCCL_MINOR   := 0
 NCCL_PATCH   := 0
-CUDA_VERSION ?= $(shell ls $(CUDA_LIB)/libcudart.so.* | head -1 | rev | cut -d "." -f -2 | rev)
+CUDA_VERSION ?= $(shell ls $(CUDA_LIB)/libcudart.so.*.*.* | rev | cut -d "." -f -3 | rev)
 CUDA_MAJOR = $(shell echo $(CUDA_VERSION) | cut -d "." -f 1)
 CUDA_MINOR = $(shell echo $(CUDA_VERSION) | cut -d "." -f 2)
+CUDA_BUILD = $(shell echo $(CUDA_VERSION) | cut -d "." -f 3)
 
-CXXFLAGS  += -DCUDA_MAJOR=$(CUDA_MAJOR) -DCUDA_MINOR=$(CUDA_MINOR)
+CXXFLAGS  += -DCUDA_MAJOR=$(CUDA_MAJOR) -DCUDA_MINOR=$(CUDA_MINOR) -DCUDA_BUILD=$(CUDA_BUILD)
