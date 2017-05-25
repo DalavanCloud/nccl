8bda1fb3d40cec5efb4944d352e27750a345a6b4
Sylvain Jeaugey <sjeaugey@nvidia.com>
2017-05-25 16:50:04 -0700

    test/perf: Fix column filtering for plots

--- test/perf/run_perf_graphs.sh
+++ test/perf/run_perf_graphs.sh
@@ -3,11 +3,7 @@
 generate_perf() {
 op=$1
 ngpus=$2
-
-if [ "$ngpus" == "" ]; then
-  echo "Usage : $0 <operation> <ngpus>"
-  exit 1
-fi
+busbwcol=$3
 
 result=$op.$ngpus
 
@@ -17,7 +13,7 @@ test/perf/${op}_perf -g $ngpus -b 5000 -e 955000 -i 5000 > results/$result.out
 test/perf/${op}_perf -g $ngpus -b 1000000 -e 19000000 -i 1000000 >> results/$result.out
 test/perf/${op}_perf -g $ngpus -b 20000000 -e 400000000 -i 20000000 >> results/$result.out
 
-cat results/$result.out | grep sum | awk '{ print $1,$12; }' > results/$result.values
+cat results/$result.out | grep float | awk "{ print \$1,\$$busbwcol; }" > results/$result.values
 
 cat > results/$result.plot << EOF
 set term png
@@ -31,8 +27,24 @@ EOF
 gnuplot results/$result.plot
 }
 
-for op in reduce broadcast all_reduce all_gather reduce_scatter; do
-  for ngpus in 2 4 6 8; do
-    generate_perf $op $ngpus
-  done
+ngpu_loop() {
+op=$1
+maxgpu=$2
+busbwcol=$3
+for ngpus in `seq 2 2 $maxgpu`; do
+  generate_perf $op $ngpus $busbwcol
 done
+}
+
+maxgpu=$1
+
+if [ "$maxgpu" == "" ]; then
+  echo "Usage : $0 <maxgpus>"
+  exit 1
+fi
+
+ngpu_loop reduce $maxgpu 12
+ngpu_loop broadcast $maxgpu 7
+ngpu_loop all_reduce $maxgpu 11
+ngpu_loop all_gather $maxgpu 10
+ngpu_loop reduce_scatter $maxgpu 11
