03edeb96b3b1a4e9fcdf6e7e30324d3112d8c4d6
Ke Wen <kwen@nvidia.com>
2017-06-02 13:52:19 -0700

    Upload test scripts on the node side

---
+++ test/scripts/nccl_test_node.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+gpumodel=$1
+
+# get dir of test scripts
+SHDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $SHDIR/../../
+NCCLROOT=$PWD
+BLDDIR=$NCCLROOT/build
+
+# build
+cd $NCCLROOT
+source $SHDIR/cuda.sh
+make -j src.build
+make -j test.build
+
+# test
+cd $BLDDIR
+LD_LIBRARY_PATH=$BLDDIR/lib:$LD_LIBRARY_PATH $SHDIR/run_perf_graphs.sh $gpumodel 8 bench
+echo "NCCL_Complete" > state
