ee0e05a9946cfd605947c3352bde9fd5306f33a1
Ke Wen <kwen@nvidia.com>
2017-06-21 09:00:01 -0700

    test/scripts: reduce test cases for reorder

--- test/scripts/run_perf_graphs.sh
+++ test/scripts/run_perf_graphs.sh
@@ -43,13 +43,17 @@ result=$resdir/$gpumodel/$op.$ngpus
 mkdir -p $resdir/$gpumodel/
 if [ "$mpi" == "0" ]; then
   echo "Running test/perf/${op}_perf on $ngpus GPUs ..."
-  eval $SET_VISIBLE srun -p $gpumodel -n 1 -c $ngpus -t ${timeout} test/perf/${op}_perf -g $ngpus -b 40000 -e 1960000 -i 40000 $extra -w 20 -n 20 | tee $result.out
-  eval $SET_VISIBLE srun -p $gpumodel -n 1 -c $ngpus -t ${timeout} test/perf/${op}_perf -t $ngpus -p 1 -b 2000000 -e 38000000 -i 2000000 $extra -w 20 -n 5 | tee -a $result.out
+  if [ "$reorder" == "0" ]; then
+    eval $SET_VISIBLE srun -p $gpumodel -n 1 -c $ngpus -t ${timeout} test/perf/${op}_perf -g $ngpus -b 40000 -e 1960000 -i 40000 $extra -w 20 -n 20 | tee $result.out
+    eval $SET_VISIBLE srun -p $gpumodel -n 1 -c $ngpus -t ${timeout} test/perf/${op}_perf -t $ngpus -p 1 -b 2000000 -e 38000000 -i 2000000 $extra -w 20 -n 5 | tee -a $result.out
+  fi
   eval $SET_VISIBLE srun -p $gpumodel -n 1 -c $ngpus -t ${timeout} test/perf/${op}_perf -t $ngpus -b 40000000 -e 400000000 -i 40000000 $extra -w 5 -n 1 | tee -a $result.out
 else
   echo "Running test/perf/${op}_perf on $ngpus GPUs [MPI] ..."
-  salloc -p $gpumodel -n $ngpus -c 1 -t ${timeout} mpirun -x NCCL_DEBUG $SET_VISIBLE -np $ngpus test/perf/${op}_perf -b 40000 -e 1960000 -i 40000 $extra -w 20 -n 20 | tee $result.out
-  salloc -p $gpumodel -n $ngpus -c 1 -t ${timeout} mpirun -x NCCL_DEBUG $SET_VISIBLE -np $ngpus test/perf/${op}_perf -b 2000000 -e 38000000 -i 2000000 $extra -w 20 -n 5 | tee -a $result.out
+  if [ "$reorder" == "0" ]; then
+    salloc -p $gpumodel -n $ngpus -c 1 -t ${timeout} mpirun -x NCCL_DEBUG $SET_VISIBLE -np $ngpus test/perf/${op}_perf -b 40000 -e 1960000 -i 40000 $extra -w 20 -n 20 | tee $result.out
+    salloc -p $gpumodel -n $ngpus -c 1 -t ${timeout} mpirun -x NCCL_DEBUG $SET_VISIBLE -np $ngpus test/perf/${op}_perf -b 2000000 -e 38000000 -i 2000000 $extra -w 20 -n 5 | tee -a $result.out
+  fi
   salloc -p $gpumodel -n $ngpus -c 1 -t ${timeout} mpirun -x NCCL_DEBUG $SET_VISIBLE -np $ngpus test/perf/${op}_perf -b 40000000 -e 400000000 -i 40000000 $extra -w 5 -n 1 | tee -a $result.out
 fi
 }
@@ -97,8 +101,12 @@ done
 
 export NCCL_DEBUG=WARN
 
-perf_ngpu_loop $gpumodel $maxgpu $check $mpi $all $reorder reduce
-perf_ngpu_loop $gpumodel $maxgpu $check $mpi $all $reorder broadcast
-perf_ngpu_loop $gpumodel $maxgpu $check $mpi $all $reorder all_reduce
-perf_ngpu_loop $gpumodel $maxgpu $check $mpi $all $reorder all_gather
-perf_ngpu_loop $gpumodel $maxgpu $check $mpi $all $reorder reduce_scatter
+if [ "$reorder" == "1" ] ; then
+  perf_ngpu_loop $gpumodel $maxgpu $check $mpi $all $reorder all_reduce
+else
+  perf_ngpu_loop $gpumodel $maxgpu $check $mpi $all $reorder reduce
+  perf_ngpu_loop $gpumodel $maxgpu $check $mpi $all $reorder broadcast
+  perf_ngpu_loop $gpumodel $maxgpu $check $mpi $all $reorder all_reduce
+  perf_ngpu_loop $gpumodel $maxgpu $check $mpi $all $reorder all_gather
+  perf_ngpu_loop $gpumodel $maxgpu $check $mpi $all $reorder reduce_scatter
+fi
