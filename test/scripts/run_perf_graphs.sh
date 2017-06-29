e88ef7a3d9d0715fa89144a124949bbf9d0242f8
Ke Wen <kwen@nvidia.com>
2017-06-29 16:59:03 -0700

    test/scripts: re-enable all collectives and all ngpus for datatype x operation tests

--- test/scripts/run_perf_graphs.sh
+++ test/scripts/run_perf_graphs.sh
@@ -36,41 +36,43 @@ if [ "$reorder" == "1" ]; then
   resdir+="_reorder"
 fi
 
-mkdir -p $resdir/$gpumodel/
-
 if [ "$SLURM" == "1" ]; then
   srun_cmd="srun -p $gpumodel -n 1 -c $ngpus -t ${timeout} "
   salloc_cmd="salloc -p $gpumodel -n $ngpus -c 1 -t ${timeout} "
 fi
 
+path=$resdir/$gpumodel
+mkdir -p $path
 
 if [ "$all" == "1" ]; then
+  mkdir -p $path/pow2
+  mkdir -p $path/npow2
   for dtype in float double half int8 int32 int64 uint8 uint32 uint64 ; do
     for otype in sum max min prod ; do
-      result=$resdir/$gpumodel/$dtype.$otype
-      $srun_cmd test/perf/${op}_perf -t $ngpus -d $dtype -o $otype -b 64 -e 4194304 -f 256 $extra -w 10 -n 10 | tee $result.pow2.out
-      $srun_cmd test/perf/${op}_perf -t $ngpus -d $dtype -o $otype -b 63 -e 4357647 -f 263 $extra -w 10 -n 10 | tee $result.npow2.out
+      result=$op.$ngpus.$dtype.$otype
+      $srun_cmd test/perf/${op}_perf -t $ngpus -d $dtype -o $otype -b 64 -e 4194304 -f 256 $extra -w 10 -n 10 2>&1 | tee $path/pow2/$result.out
+      $srun_cmd test/perf/${op}_perf -t $ngpus -d $dtype -o $otype -b 63 -e 4357647 -f 263 $extra -w 10 -n 10 2>&1 | tee $path/npow2/$result.out
     done
   done
   return 0
 fi
 
-result=$resdir/$gpumodel/$op.$ngpus
+result=$path/$op.$ngpus
 
 if [ "$mpi" == "0" ]; then
   echo "Running test/perf/${op}_perf on $ngpus GPUs ..."
   if [ "$reorder" == "0" ]; then
-    $srun_cmd test/perf/${op}_perf -t $ngpus -b 40000 -e 1960000 -i 40000 $extra -w 20 -n 20 | tee $result.out
-    $srun_cmd test/perf/${op}_perf -t $ngpus -p 1 -b 2000000 -e 38000000 -i 2000000 $extra -w 20 -n 5 | tee -a $result.out
+    $srun_cmd test/perf/${op}_perf -t $ngpus -b 40000 -e 1960000 -i 40000 $extra -w 20 -n 20 2>&1 | tee $result.out
+    $srun_cmd test/perf/${op}_perf -t $ngpus -p 1 -b 2000000 -e 38000000 -i 2000000 $extra -w 20 -n 5 2>&1 | tee -a $result.out
   fi
-  eval $SET_VISIBLE $srun_cmd test/perf/${op}_perf -g $ngpus -b 40000000 -e 400000000 -i 40000000 $extra -w 5 -n 1 | tee -a $result.out
+  eval $SET_VISIBLE $srun_cmd test/perf/${op}_perf -g $ngpus -b 40000000 -e 400000000 -i 40000000 $extra -w 5 -n 1 2>&1 | tee -a $result.out
 else
   echo "Running test/perf/${op}_perf on $ngpus GPUs [MPI] ..."
   if [ "$reorder" == "0" ]; then
-    $salloc_cmd mpirun -x NCCL_DEBUG -np $ngpus test/perf/${op}_perf -b 40000 -e 1960000 -i 40000 $extra -w 20 -n 20 | tee $result.out
-    $salloc_cmd mpirun -x NCCL_DEBUG -np $ngpus test/perf/${op}_perf -b 2000000 -e 38000000 -i 2000000 $extra -w 20 -n 5 | tee -a $result.out
+    $salloc_cmd mpirun -x NCCL_DEBUG -np $ngpus test/perf/${op}_perf -b 40000 -e 1960000 -i 40000 $extra -w 20 -n 20 2>&1 | tee $result.out
+    $salloc_cmd mpirun -x NCCL_DEBUG -np $ngpus test/perf/${op}_perf -b 2000000 -e 38000000 -i 2000000 $extra -w 20 -n 5 2>&1 | tee -a $result.out
   fi
-  $salloc_cmd mpirun -x NCCL_DEBUG $SET_VISIBLE -np $ngpus test/perf/${op}_perf -b 40000000 -e 400000000 -i 40000000 $extra -w 5 -n 1 | tee -a $result.out
+  $salloc_cmd mpirun -x NCCL_DEBUG $SET_VISIBLE -np $ngpus test/perf/${op}_perf -b 40000000 -e 400000000 -i 40000000 $extra -w 5 -n 1 2>&1 | tee -a $result.out
 fi
 }
 
@@ -119,8 +121,6 @@ export NCCL_DEBUG=WARN
 
 if [ "$reorder" == "1" ] ; then
   perf_ngpu_loop $gpumodel $maxgpu $check $mpi $all $reorder all_reduce
-elif [ "$all" == "1" ] ; then
-  generate_perf  $gpumodel $maxgpu $check $mpi $all $reorder all_reduce
 else
   perf_ngpu_loop $gpumodel $maxgpu $check $mpi $all $reorder reduce
   perf_ngpu_loop $gpumodel $maxgpu $check $mpi $all $reorder broadcast
