c0fd68a24c88eeb8908db679641983c869d0ba8f
Ke Wen <kwen@nvidia.com>
2017-08-29 11:07:25 -0700

    test/scripts: add quick test (use mode code 'combo')

--- test/scripts/run_perf_graphs.sh
+++ test/scripts/run_perf_graphs.sh
@@ -6,13 +6,6 @@ ngpus=$2
 mode=$3
 op=$4
 
-resdir="results"
-if [ "$mode" != "single" ]; then
-  resdir+="_$mode"
-fi
-path=$resdir/$gpumodel
-mkdir -p $path
-
 timeout=2
 extra="-c 1 "
 if [ "$mode" == "all" ]; then
@@ -28,56 +21,89 @@ else
   mpi_hosts="-host $gpumodel "
 fi
 
-if [ "$mode" == "reorder" ]; then
+if [ "$mode" == "reorder" ] || [ "$mode" == "combo" ] ; then
   echo "Running test/perf/${op}_perf on $ngpus GPUs [Reorder] ..."
+  resdir="results_reorder"
+  path=$resdir/$gpumodel
+  mkdir -p $path
   GPU_REORDER=""
   for gpu in `seq $ngpus -1 0`; do
     GPU_REORDER+=",$gpu"
   done
   GPU_REORDER=`echo $GPU_REORDER | cut -c 4-`
   result=$path/$op.$ngpus
-  CUDA_VISIBLE_DEVICES=$GPU_REORDER $srun_cmd test/perf/${op}_perf -g $ngpus -b 40000000 -e 400000000 -i 40000000 -w 1 -n 5 2>&1 | tee -a $result.out
-  return 0
+  if [ "$mode" == "combo" ]; then
+    CUDA_VISIBLE_DEVICES=$GPU_REORDER $srun_cmd test/perf/${op}_perf -g $ngpus -b 40000000 -e 80000000 -i 40000000 -w 1 -n 5 2>&1 | tee $result.out
+  else
+    CUDA_VISIBLE_DEVICES=$GPU_REORDER $srun_cmd test/perf/${op}_perf -g $ngpus -b 40000000 -e 400000000 -i 40000000 -w 1 -n 5 2>&1 | tee $result.out
+    return 0
+  fi
 fi
 
-if [ "$mode" == "all" ]; then
+if [ "$mode" == "all" ] || [ "$mode" == "combo" ]; then
+  resdir="results_all"
+  path=$resdir/$gpumodel
   mkdir -p $path/pow2
   mkdir -p $path/npow2
+  if [ "$mode" == "combo" ]; then
+    otypes="sum"
+  else
+    otypes="sum max min prod"
+  fi
   for dtype in float double half int8 int32 int64 uint8 uint32 uint64 ; do
-    for otype in sum max min prod ; do
+    for otype in $otypes ; do
       echo "Running test/perf/${op}_perf on $ngpus GPUs [$dtype x $otype] ..."
       result=$op.$ngpus.$dtype.$otype
-      $srun_cmd test/perf/${op}_perf -t $ngpus -d $dtype -o $otype -b 64 -e 4194304 -f 256 -w 1 -n 5 2>&1 | tee $path/pow2/$result.out
-      $srun_cmd test/perf/${op}_perf -t $ngpus -d $dtype -o $otype -b 63 -e 4357647 -f 263 -w 1 -n 5 2>&1 | tee $path/npow2/$result.out
+      if [ "$mode" != "combo" ]; then
+        $srun_cmd test/perf/${op}_perf -t $ngpus -p 1 -d $dtype -o $otype -b 64 -e 4194304 -f 256 -w 1 -n 5 2>&1 | tee $path/pow2/$result.out
+      fi
+      $srun_cmd test/perf/${op}_perf -t $ngpus -p 1 -d $dtype -o $otype -b 63 -e 4357647 -f 263 -w 1 -n 5 2>&1 | tee $path/npow2/$result.out
     done
   done
-  return 0
+  if [ "$mode" != "combo" ]; then
+    return 0
+  fi
 fi
 
-if [ "$mode" == "single" ]; then
+if [ "$mode" == "single" ] || [ "$mode" == "combo" ]; then
   echo "Running test/perf/${op}_perf on $ngpus GPUs ..."
+  resdir="results"
+  path=$resdir/$gpumodel
+  mkdir -p $path
   result=$path/$op.$ngpus
   $srun_cmd test/perf/${op}_perf -t $ngpus -b 40000 -e 1960000 -i 40000 $extra -w 20 -n 20 2>&1 | tee $result.out
-  $srun_cmd test/perf/${op}_perf -t $ngpus -p 1 -b 2000000 -e 38000000 -i 2000000 $extra -w 20 -n 5 2>&1 | tee -a $result.out
-  $srun_cmd test/perf/${op}_perf -g $ngpus -b 40000000 -e 400000000 -i 40000000 $extra -w 5 -n 1 2>&1 | tee -a $result.out
-  return 0
+  if [ "$mode" != "combo" ]; then
+    $srun_cmd test/perf/${op}_perf -t $ngpus -p 1 -b 2000000 -e 38000000 -i 2000000 $extra -w 20 -n 5 2>&1 | tee -a $result.out
+    $srun_cmd test/perf/${op}_perf -g $ngpus -b 40000000 -e 400000000 -i 40000000 $extra -w 5 -n 1 2>&1 | tee -a $result.out
+    return 0
+  fi
 fi
 
-if [ "$mode" == "latency" ]; then
+if [ "$mode" == "latency" ] || [ "$mode" == "combo" ]; then
   echo "Running test/perf/${op}_perf on $ngpus GPUs [Latency] ..."
+  resdir="results_latency"
+  path=$resdir/$gpumodel
+  mkdir -p $path
   result=$path/$op.$ngpus
   $srun_cmd test/perf/${op}_perf -t $ngpus -b 32 -e 1K -f 2 -w 20 -n 20 2>&1 | tee $result.out
   $srun_cmd test/perf/${op}_perf -g $ngpus -b 2K -e 64K -f 2 -w 20 -n 20 2>&1 | tee -a $result.out
-  return 0
+  if [ "$mode" != "combo" ]; then
+    return 0
+  fi
 fi
 
-if [ "$mode" == "mpi" ]; then
+if [ "$mode" == "mpi" ] || [ "$mode" == "combo" ]; then
   echo "Running test/perf/${op}_perf on $ngpus GPUs [MPI] ..."
+  resdir="results_mpi"
+  path=$resdir/$gpumodel
+  mkdir -p $path
   result=$path/$op.$ngpus
   $salloc_cmd mpirun $mpi_hosts -x NCCL_DEBUG -np $ngpus test/perf/${op}_perf -b 40000 -e 1960000 -i 40000 -w 20 -n 20 2>&1 | tee $result.out
-  $salloc_cmd mpirun $mpi_hosts -x NCCL_DEBUG -np $ngpus test/perf/${op}_perf -b 2000000 -e 38000000 -i 2000000  -w 20 -n 5 2>&1 | tee -a $result.out
-  $salloc_cmd mpirun $mpi_hosts -x NCCL_DEBUG -np $ngpus test/perf/${op}_perf -b 40000000 -e 400000000 -i 40000000 -w 5 -n 1 2>&1 | tee -a $result.out
-  return 0
+  if [ "$mode" != "combo" ]; then
+    $salloc_cmd mpirun $mpi_hosts -x NCCL_DEBUG -np $ngpus test/perf/${op}_perf -b 2000000 -e 38000000 -i 2000000  -w 20 -n 5 2>&1 | tee -a $result.out
+    $salloc_cmd mpirun $mpi_hosts -x NCCL_DEBUG -np $ngpus test/perf/${op}_perf -b 40000000 -e 400000000 -i 40000000 -w 5 -n 1 2>&1 | tee -a $result.out
+    return 0
+  fi
 fi
 }
 
