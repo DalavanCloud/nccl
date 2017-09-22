a9ce52bee0a8c30fa0662723858d8b83de97d6cc
Sylvain Jeaugey <sjeaugey@nvidia.com>
2017-09-22 11:21:40 -0700

    Add suffix and set version to 2.1.0~a0-1

--- makefiles/version.mk
+++ makefiles/version.mk
@@ -1,7 +1,8 @@
 ##### version
 NCCL_MAJOR   := 2
-NCCL_MINOR   := 0
-NCCL_PATCH   := 5
+NCCL_MINOR   := 1
+NCCL_PATCH   := 0
+NCCL_SUFFIX  := ~a0
 CUDA_VERSION ?= $(shell ls $(CUDA_LIB)/libcudart.so.* | head -1 | rev | cut -d "." -f -2 | rev)
 CUDA_MAJOR = $(shell echo $(CUDA_VERSION) | cut -d "." -f 1)
 CUDA_MINOR = $(shell echo $(CUDA_VERSION) | cut -d "." -f 2)
