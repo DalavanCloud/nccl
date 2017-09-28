73c8db000dd0d85a714b09da89014bcb0e348afd
Sylvain Jeaugey <sjeaugey@nvidia.com>
2017-09-28 14:58:20 -0700

    2.1.0~b0

--- makefiles/version.mk
+++ makefiles/version.mk
@@ -2,7 +2,7 @@
 NCCL_MAJOR   := 2
 NCCL_MINOR   := 1
 NCCL_PATCH   := 0
-NCCL_SUFFIX  := ~a0
+NCCL_SUFFIX  := ~b0
 CUDA_VERSION ?= $(shell ls $(CUDA_LIB)/libcudart.so.* | head -1 | rev | cut -d "." -f -2 | rev)
 CUDA_MAJOR = $(shell echo $(CUDA_VERSION) | cut -d "." -f 1)
 CUDA_MINOR = $(shell echo $(CUDA_VERSION) | cut -d "." -f 2)
