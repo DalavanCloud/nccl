bb81fb861ba9b07beef78058d8785f8c8f4a243d
Anshuman Goswami <agoswami@nvidia.com>
2017-10-06 19:21:50 -0700

    test only mx(w & w/o NCCL) and C2(w NCCL)

--- test/scripts/nccl_test_node.sh
+++ test/scripts/nccl_test_node.sh
@@ -45,9 +45,9 @@ if [ "$mode" == "dlfw" ] && [ "$gpumodel" == "P100" ]; then
   cd $BLDDIR
   $SHDIR/mxnet.sh $gpumodel
   $SHDIR/caffe2.sh $gpumodel
-  $SHDIR/tensorflow.sh $gpumodel
-  $SHDIR/pytorch.sh $gpumodel
-  $SHDIR/cntk.sh $gpumodel
+  #$SHDIR/tensorflow.sh $gpumodel
+  #$SHDIR/pytorch.sh $gpumodel
+  #$SHDIR/cntk.sh $gpumodel
 elif [[ "$mode" == *"mpi"* ]] || [[ "$mode" == *"multinode"* ]]; then
   # test (multi processes)
   cd $NCCLROOT
